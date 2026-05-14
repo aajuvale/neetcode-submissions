44daa437a79a1d3c8c005c0681aaf0a7f8243fe1
Ahmed <66859813+aajuvale@users.noreply.github.com>
2026-05-14 14:05:55 -0700

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var (L, R) = (0, nums.count - 1)
+
+        while L <= R {
+            let midpoint = (L + R) / 2
+            if nums[midpoint] < target {
+                L = midpoint + 1
+            } else if nums[midpoint] > target {
+                R = midpoint - 1
+            } else {
+                return midpoint
+            }
+        }
+
+        return -1
+    }
+}
